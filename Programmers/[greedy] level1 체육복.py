d9d83d3d9bca1595471f2280634286a1e24afeda
Choi Jung Woo <choi-ju12g@naver.com>
2021-10-16 23:25:53 +0900

    add code that lost and reserve 's same value remove

--- Programmers/[greedy] level1 체육복.py
+++ Programmers/[greedy] level1 체육복.py
@@ -1,17 +1,28 @@
 def solution(n, lost, reserve):
     answer = 0
     count = 0
-    for i in range(len(lost)):
-        a, b = lost[i] - 1, lost[i] + 1
-        for j in range(len(reserve)):
-            if reserve[j] == a:
-                count += 1
-                del reserve[j]
-                break
-            elif reserve[j] == b:
-                count += 1
-                del reserve[j]
-                break
 
-    answer = n - len(lost) + count
+    # lost와 reserve 동일한 값 제거
+    lost = list(set(lost) - set(reserve))
+    reserve = list(set(reserve) - set(lost))
+
+    # 빌려줄 수 있는 수 계산
+    answer = n - len(lost)
+    for i in range(len(lost)):
+        a = lost.pop()
+
+        check = False
+        for j in range(len(reserve)):
+            if reserve[j] == a - 1:
+                del reserve[j]
+                check = True
+                count += 1
+                break
+        if not check:
+            for j in range(len(reserve)):
+                if reserve[j] == a + 1:
+                    del reserve[j]
+                    count += 1
+                    break
+    answer += count
     return answer
