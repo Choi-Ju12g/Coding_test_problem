a6a5b2b28d7ed55fbb830b836e9f5f17c3ccaac1
Choi Jung Woo <choi-ju12g@naver.com>
2021-10-17 14:30:52 +0900

    can't clear 11 test case, so learn answer directly

--- Programmers/[greedy] level2 조이스틱.py
+++ Programmers/[greedy] level2 조이스틱.py
@@ -9,14 +9,34 @@ def solution(name):
         else:
             charset.append(26-int(ord(name[i])) + standard)
             answer += 26-int(ord(name[i])) + standard
-    print("up down"+str(answer))
+    #print("up down"+str(answer))
     if 0 not in charset:
         answer += len(name)-1
     else:
         next = charset.index(0) +1
         while next < len(name) and charset[next] == 0:
             next += 1
-        answer += min(len(name)-1, (2*charset.index(0)+len(name)-next))
+        if charset.index(0) > 1:
+            answer += min(len(name)-1, (2*charset.index(0)+len(name)-next))
+        else:
+            answer += min(len(name)-1, len(name)-next)
     return answer
 
-print(solution("JAN"))
+print(solution("ABAAAAAAAAABB"))
+
+# 모범답안
+def solution(name):
+    answer = 0
+    min_move = len(name) - 1
+    next = 0
+
+    for i, char in enumerate(name):
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+
+        min_move = min(min_move, i + i + len(name) - next)
+    answer += min_move
+    return answer
