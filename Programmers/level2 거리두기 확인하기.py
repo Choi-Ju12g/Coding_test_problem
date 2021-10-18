293eaf277aec9b10b45b7cac7a636c3cb9e04974
Choi Jung Woo <choi-ju12g@naver.com>
2021-10-19 00:06:50 +0900

    add annotation 거리두기 확인하기

--- Programmers/level2 거리두기 확인하기.py
+++ Programmers/level2 거리두기 확인하기.py
@@ -2,7 +2,7 @@ from collections import deque
 
 move =[(1,0),(0,-1),(-1,0),(0,1)]
 
-# place : 5*5 matrix, x:시작 위치 x좌표, y:시작 위치 y좌표
+# place : 5*5 matrix, x:시작 위치 x좌표, y:시작 위치 y좌표 // true : 거리두기 완료, false : 거리두기 실패
 def bfs(place,x,y):
     visited = [[0]*5 for i in range(5)]
     que = deque()
@@ -12,9 +12,11 @@ def bfs(place,x,y):
     while que:
         pos = que.popleft()
 
+        # 거리 1 또는 2 에  p(참가자) 있을때
         if (pos[2] == 1 or pos[2] == 2) and place[pos[0]][pos[1]] == 'P':
             return False
 
+        # 상하좌우에 대한 다음 값 저장후, 이동시켜보기
         for i in range(len(move)):
             #매번 다음 위치 값 저장
             dPos =[0,0,0]
@@ -24,6 +26,7 @@ def bfs(place,x,y):
 
             # 맵 범위 벗어나는 경우 제외
             if 0<= dPos[0] < 5 and 0<= dPos[1]<5:
+                # 다음 위치가 가림막이 아니고, 참가자가 앉을 수 있는 자리인 경우 : 그 자리를 que에 또 넣음, 방문처리
                 if place[dPos[0]][dPos[1]] != "X" and visited[dPos[0]][dPos[1]] ==0:
                     que.append(dPos)
                     visited[dPos[0]][dPos[1]] = 1
@@ -34,7 +37,6 @@ def bfs(place,x,y):
 
 def solution(places):
     answer = []
-    bool = True
     for place in places:
         bool = True
         for x in range(len(place)):
