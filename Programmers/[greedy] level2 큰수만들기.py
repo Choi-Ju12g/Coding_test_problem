91a5f44f8772d43b37cde8adbc5a7a69a0367bfd
Choi Jung Woo <choi-ju12g@naver.com>
2021-10-18 21:16:03 +0900

    combinations over timelimits

--- Programmers/[greedy] level2 큰수만들기.py
+++ Programmers/[greedy] level2 큰수만들기.py
@@ -1,10 +1,23 @@
+from itertools import combinations
+
 def solution(number, k):
     answer = ''
-    temp = number
+    list_comb = list(combinations(list(str(number)),len(str(number))-k))
+    #print(list_comb)
+    list_comb.sort(reverse=True)
+    answer = ''.join(list_comb[0])
+    return answer
+
+
+
+def solution_def(number,k):
+    answer = ''
+    temp = int(number)
     sort_list = []
     while temp > 0:
-        sort_list.append(temp%10)
+        sort_list.append(temp % 10)
         temp //= 10
+
     sort_list.sort(reverse=True)
     for i in range(k):
         sort_list.pop()
