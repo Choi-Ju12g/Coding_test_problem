13739a33821f0e89477f9402d6e91b02c680cc90
Choi-Ju12g <choi-ju12g@naver.com>
2022-01-11 11:02:57 +0900

    dfs bfs ex

--- This is coding_test/dfs&bfs.py
+++ This is coding_test/dfs&bfs.py
@@ -32,7 +32,6 @@ def bfs(graph, start_vertex,visited):
                 visited[i] = True
 
 
-
 # 이 그래프는 인접 리스트 방식
 graph = [[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]]
 visited = [False]*9
@@ -40,3 +39,55 @@ dfs(graph,1,visited)
 print()
 visited2 = [False]*9
 bfs(graph,1,visited2)
+
+
+
+
+
+
+
+
+
+
+
+#################################################
+graph_list = {1: set([3, 4]),
+              2: set([3, 4, 5]),
+              3: set([1, 5]),
+              4: set([1]),
+              5: set([2, 6]),
+              6: set([3, 5])}
+root_node = 1
+
+
+from collections import deque
+
+
+def BFS_with_adj_list(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            queue += graph[n] - set(visited)
+    return visited
+
+
+print(BFS_with_adj_list(graph_list, root_node))
+
+##########################################################
+def DFS_with_adj_list(graph, root):
+    visited = []
+    stack = [root]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack += graph[n] - set(visited)
+    return visited
+
+print(BFS_with_adj_list(graph_list, root_node))
+##################################################
